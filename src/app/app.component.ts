ac5bb2eaa1542cef9bc90e2a417eafc7afacddec
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-20 10:23:22 +0530

    Authentication Worked

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { UserdataService } from './service/userdata.service';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { Subscription } from 'rxjs';
+import { OnDestroy } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'VerifyToolTest';
+export class AppComponent implements OnDestroy {
+  loggedin = false;
+  subAuth: Subscription;
+
+  constructor(public afAuth: AngularFireAuth, public tutorialService: UserdataService) {
+
+    this.subAuth = this.afAuth.authState.subscribe(res => {
+      if (res && res.uid) {
+        this.loggedin = true;
+      } else {
+        this.loggedin = false;
+      }
+    });
+  }
+
+  
+  ngOnDestroy()
+  {
+    this.subAuth.unsubscribe();
+  }
 }
--- src/app/service/userdata.service.ts
+++ src/app/service/userdata.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import firebase from 'firebase/app';
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserdataService {
-
-  constructor() { }
+  constructor(public auth: AngularFireAuth){}
+  login() {
+    return this.auth.signInWithPopup( new (firebase.auth as any).GoogleAuthProvider());
+  }
+  logout() {
+    return this.auth.signOut();
+  }
 }

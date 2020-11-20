fb5adbc234021b8bf6962b2dd5cf6be626b77f73
merlinpratheesh <merlinpratheesh@gmail.com>
2020-11-20 10:34:51 +0530

    initialCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { UserdataService } from './service/userdata.service';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Subscription } from 'rxjs';
 import { OnDestroy } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,7 +12,7 @@ import { OnDestroy } from '@angular/core';
 export class AppComponent implements OnDestroy {
   loggedin = false;
   subAuth: Subscription;
-
+  myarraydisplay: [] = [];
   constructor(public afAuth: AngularFireAuth, public tutorialService: UserdataService) {
 
     this.subAuth = this.afAuth.authState.subscribe(res => {
@@ -21,6 +22,15 @@ export class AppComponent implements OnDestroy {
         this.loggedin = false;
       }
     });
+    this.subAuth = this.afAuth.authState.subscribe(res => {
+      if (res && res.uid) {
+        this.tutorialService.getDocumentData('TestAngular','TestMain', 'TestSub').subscribe((some)=>{
+          if(some !==null){
+              this.myarraydisplay= some.data();
+          }
+        });
+        }
+});
   }
 
   
--- src/app/service/userdata.service.ts
+++ src/app/service/userdata.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import firebase from 'firebase/app';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { collectionData, doc } from 'rxfire/firestore';
 
 
 
@@ -8,11 +10,16 @@ import firebase from 'firebase/app';
   providedIn: 'root'
 })
 export class UserdataService {
-  constructor(public auth: AngularFireAuth){}
+  constructor(public auth: AngularFireAuth, private db: AngularFirestore){}
   login() {
     return this.auth.signInWithPopup( new (firebase.auth as any).GoogleAuthProvider());
   }
   logout() {
     return this.auth.signOut();
+  }
+  getDocumentData(projectname:string, mainfield: string, subfield: string): any {
+    const collectionPath= projectname + '/' + mainfield + '/TestItems/'+ subfield;  
+    const myDocRef = this.db.firestore.doc(collectionPath);            
+    return doc(myDocRef);
   }
 }
